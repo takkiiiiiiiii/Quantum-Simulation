a7217957bc7e936ebd18a29f54373ee03ffe68bc
takkiiiiiiiii <yudai2002brazil@gmail.com>
2023-07-16 10:36:06 +0900

    BB84 completed

--- chapter3/bb84.py
+++ chapter3/bb84.py
@@ -1,5 +1,9 @@
 from unittest import result
+from simulator import SingleQubitSimulator
 from interface import QuantumDevice, Qubit
+
+
+# helper functions before the key exchange
 
 def sample_random_bit(device: QuantumDevice) -> bool:
     with device.using_qubit() as q:
@@ -10,8 +14,8 @@ def sample_random_bit(device: QuantumDevice) -> bool:
 
 def prepare_message_qubit(message: bool, basis: bool, q: Qubit) -> None:  #The qubit is encoded with the key bit value in the randomly selected basis.
     if message:
-        q.x()
-    if basis:
+        q.x()   # メッセージの時は、X演算を適用
+    if basis:   # 基底の時は、ハダマード演算を適用
         q.h()  
 
 def measure_message_qubit(basis: bool, q: Qubit) -> bool:
@@ -28,6 +32,7 @@ def convert_to_hex(bits: List[bool]) -> str:
     ))
 
 # BB84 protocol for sending a classical bit
+
 def send_single_bit_with_bb84 (
     your_device: QuantumDevice,
     eve_device: QuantumDevice
@@ -45,4 +50,60 @@ def send_single_bit_with_bb84 (
 
     eve_result = measure_message_qubit(eve_basis, q)
 
+
+    # Returns the key bit values and bases we and Eve would have at the end of this one round
     return ((your_message, your_basis), (eve_result, eve_basis))
+
+def simulate_bb84(n_bits: int) -> list:
+    your_device = SingleQubitSimulator()
+    eve_device = SingleQubitSimulator()
+
+    key = []
+    n_rounds = 0
+
+    while len(key) < n_bits:
+        n_rounds += 1
+        ((your_message, your_basis), (eve_result, eve_basis)) = \
+            send_single_bit_with_bb84(your_device, eve_device)
+
+        if your_basis == eve_basis:
+            assert your_message == eve_result
+            key.append(your_message)
+
+    print(f"Took {n_rounds} rounds to generate a {n_bits}-bit key.")
+
+    return key
+
+def apply_one_time_pad(message: List[bool], key: List[bool]) -> List[bool]:
+    return [
+        message_bit ^ key_bit
+        for (message_bit, key_bit) in zip(message, key)
+    ]
+
+if __name__ == "__main__":
+    print("Generating a 96-bit key by simulating BB84...")
+    key = simulate_bb84(96)
+    print(f"Got key                           {convert_to_hex(key)}.")
+
+    message = [
+        1, 1, 0, 1, 1, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 0, 1,
+        1, 1, 0, 1, 1, 1, 0, 0,
+        1, 0, 0, 1, 0, 1, 1, 0,
+        1, 1, 0, 1, 1, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 0, 1,
+        1, 1, 0, 1, 1, 1, 0, 0,
+        0, 0, 0, 0, 1, 1, 0, 1,
+        1, 1, 0, 1, 1, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 0, 1,
+        1, 1, 0, 1, 1, 1, 0, 0,
+        1, 0, 1, 1, 1, 0, 1, 1
+    ]
+    print(f"Using key to send secret message: {convert_to_hex(message)}.")
+
+    encrypted_message = apply_one_time_pad(message, key)
+    print(f"Encrypted message:                {convert_to_hex(encrypted_message)}.")
+
+    decrypted_message = apply_one_time_pad(encrypted_message, key)
+    print(f"Eve decrypted to get:             {convert_to_hex(decrypted_message)}.")
+     
--- chapter3/qkd.py
+++ chapter3/qkd.py
@@ -1,31 +1,39 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+##
+# qkd.py: Defines functions needed to perform a basic quantum key exchange protocol.
+
+
 from interface import QuantumDevice, Qubit
 from simulator import SingleQubitSimulator
 
-
 def prepare_classical_message(bit: bool, q: Qubit) -> None:
-    if bit:      #If we are sending 1, we can use the NOT Operation x.(rotating |0⟩ to |1⟩)
-        q.x()   # implementing x operation(|0⟩ to |1⟩ and vice verse)
+    if bit:
+        q.x()
 
-def eve_measure(q: Qubit) -> bool:  # Eve(receiver) receive a qubit and measure it, and then record the classical measurement result
-    q.h() # Eve measure in the X-axis because she does a Hadamard gate on her qubit before measuring.
-    return q.measure()     
+def eve_measure(q: Qubit) -> bool:
+    return q.measure()
 
-def send_classical_bit(device: QuantumDevice, bit:bool) -> None:
+def send_classical_bit(device: QuantumDevice, bit: bool) -> None:
     with device.using_qubit() as q:
         prepare_classical_message(bit, q)
         result = eve_measure(q)
         q.reset()
-    assert result == bit 
+    assert result == bit
+
+
+def qrng(device: QuantumDevice) -> bool:
+    with device.using_qubit() as q:
+        q.h()
+        return q.measure()
 
 def prepare_classical_message_plusminus(bit: bool, q: Qubit) -> None:
-    if bit:   #If we are sending 1, we can use the NOT Operation x.(rotating |0⟩ to |1⟩)
-        q.x()   # implementing x operation(|0⟩ to |1⟩ and vice verse)
-    q.h()  # Applying the Hadamard gate at this point rotates the |0⟩ / |1⟩ states to |+⟩ / |–⟩ states.
+    if bit:
+        q.x()
+    q.h()
 
-# Uses the h operation to rotate our |+⟩ / |-⟩ states back to the |0⟩ / |1⟩ states 
-# because our measure operation is defined to only measure the |0⟩ / |1⟩ states correctly
 def eve_measure_plusminus(q: Qubit) -> bool:
-    q.h()  # Eve measure in the X-axis because she does a Hadamard gate on her qubit before measuring.
+    q.h()
     return q.measure()
 
 def send_classical_bit_plusminus(device: QuantumDevice, bit: bool) -> None:
@@ -34,22 +42,8 @@ def send_classical_bit_plusminus(device: QuantumDevice, bit: bool) -> None:
         result = eve_measure_plusminus(q)
         assert result == bit
 
-
-#The function does not return anything, so if we and Eve end up with key bits that don’t match, it will raise an error.
 def send_classical_bit_wrong_basis(device: QuantumDevice, bit: bool) -> None:
     with device.using_qubit() as q:
         prepare_classical_message(bit, q)
         result = eve_measure_plusminus(q)
         assert result == bit, "Two parties do not have the same bit value"
-
-
-def qrng(device: QuantumDevice) -> bool:
-    with device.using_qubit() as q:
-        q.h()
-        return q.measure()
-
-if __name__ == "__main__":
-    qsim = SingleQubitSimulator()
-    for idx_sample in range(10):
-        random_sample = qrng(qsim)
-        print(f"Our QRNG returned {random_sample}.")
